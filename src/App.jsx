a272d6df09516a0d51b2e8b21cfb567126adb4bf
mikuweb <mxxx.kwtmg@gmail.com>
2023-01-17 10:50:45 -0800

    Add light and dark mode

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,21 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { useState } from "react";
+import { Main } from "./components/Main";
+import { Navbar } from "./components/Navbar";
+import "./style.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [darkMode, setDarkMode] = useState(false);
+
+  const toggleDarkMode = () => {
+    setDarkMode((preToggle) => !preToggle);
+  };
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <div className="container">
+      <Navbar toggleDarkMode={toggleDarkMode} darkMode={darkMode} />
+      <Main darkMode={darkMode} />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
